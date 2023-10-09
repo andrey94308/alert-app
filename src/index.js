21b51a78dd9b9b0ce6d40a6ff0433e954a059c83
andrey94308 <android9430802@ya.ru>
2023-10-10 01:47:00 +0200

    db config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from "react";
 import App from "./App";
 import "./index.css";
 import { createRoot } from 'react-dom/client';
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth'; // Дополнительные модули Firebase, если необходимо
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5SK0FM-yO4ZWPS7F5_lfBtmSJf8XmAdc",
+  authDomain: "alert-app-6fed8.firebaseapp.com",
+  projectId: "alert-app-6fed8",
+  storageBucket: "alert-app-6fed8.appspot.com",
+  messagingSenderId: "828718563802",
+  appId: "1:828718563802:web:b51cdb5c55e4d2824800e3",
+  measurementId: "G-NVPRKPS745"
+};
+
+initializeApp(firebaseConfig);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
