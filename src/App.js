92af1d939c00ee48018f0af8cc7d1705e3b50f6d
andrey94308 <android9430802@ya.ru>
2023-10-10 00:32:45 +0200

    fix 10

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import './App.css';
-import Screen1 from './components/Screen1';
-import Screen2 from './components/Screen2';
-import Screen3 from './components/Screen3';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { UserProvider } from './UserContext';
+import React from "react";
+import "./App.css";
+import Screen1 from "./components/Screen1";
+import Screen2 from "./components/Screen2";
+import Screen3 from "./components/Screen3";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { UserProvider } from "./UserContext";
 
 function App() {
   return (
     <UserProvider>
       <div className="App">
         <Router>
-          <Route path="/" exact component={Screen1} />
-          <Route path="/user-status/:id" component={Screen2} />
-          <Route path="/thanks" component={Screen3} />
+          <Routes>
+            <Route path="/" exact component={Screen1} />
+            <Route path="/user-status/:id" component={Screen2} />
+            <Route path="/thanks" component={Screen3} />
+          </Routes>
         </Router>
       </div>
     </UserProvider>
