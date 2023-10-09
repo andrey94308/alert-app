82211619394b7546270ae99ed70146b4ab0ef991
andrey94308 <android9430802@ya.ru>
2023-10-10 00:25:30 +0200

    fixed error

--- src/components/Screen3.js
+++ src/components/Screen3.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Screen3 = () => {
-  const history = useHistory();
+  const navigate = useNavigate();
 
   const goBack = () => {
-    history.goBack();
+    navigate(-1);
   };
 
   return (
