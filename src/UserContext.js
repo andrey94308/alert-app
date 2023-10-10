20bcf2be3702521e994fef024dc6c59fbeeee3bb
andrey94308 <android9430802@ya.ru>
2023-10-10 02:43:25 +0200

    working!

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,19 +1,58 @@
-import React, { createContext, useContext, useState } from "react";
-import firebase from "firebase/app";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  where,
+  query,
+  getDocs,
+  deleteDoc,
+  addDoc,
+  updateDoc,
+} from "firebase/firestore";
+
 import "firebase/database";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD5SK0FM-yO4ZWPS7F5_lfBtmSJf8XmAdc",
+  authDomain: "alert-app-6fed8.firebaseapp.com",
+  projectId: "alert-app-6fed8",
+  storageBucket: "alert-app-6fed8.appspot.com",
+  messagingSenderId: "828718563802",
+  appId: "1:828718563802:web:b51cdb5c55e4d2824800e3",
+  measurementId: "G-NVPRKPS745",
+};
+
 const UserContext = createContext();
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export const useUsers = () => {
   return useContext(UserContext);
 };
 
 export const UserProvider = ({ children }) => {
+  // const usersFromDb = await getUsersFromDb();
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true); // Loading state
 
-  const addUser = (name) => {
+  useEffect(() => {
+    const getUsersFromDb = async () => {
+      const usersCol = collection(db, "users");
+      const usersSnapshot = await getDocs(usersCol);
+      const usersList = usersSnapshot.docs.map((doc) => doc.data());
+      setUsers(usersList);
+      setLoading(false); // Set loading to false once data is fetched
+    };
+
+    getUsersFromDb(); // Call the function to fetch users
+
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  const addUser = async (name) => {
     // Reference to your table named 'users'
-    const usersRef = firebase.database().ref("users");
+    const usersRef = collection(db, "users");
 
     const newUser = {
       id: Math.random().toString(36).substr(2, 9),
@@ -23,20 +62,53 @@ export const UserProvider = ({ children }) => {
       comment: "",
     };
     setUsers((prevUsers) => [...prevUsers, newUser]);
-    usersRef.push(newUser);
+
+    try {
+      const docRef = await addDoc(usersRef, newUser);
+      console.log("User added with document ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding user: ", e);
+    }
   };
 
-  const deleteUser = (id) => {
-    const userRef = firebase.database().ref(`users/${id}`);
-    userRef.remove();
-    
+  const deleteUser = async (id) => {
+    const usersRef = collection(db, "users");
+
+    // Create a query to find the document with the matching userId
+    const q = query(usersRef, where("id", "==", id));
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach(async (doc) => {
+        await deleteDoc(doc.ref);
+        console.log(`Deleted user with id: ${id}`);
+      });
+    } catch (e) {
+      console.error("Error deleting user document: ", e);
+    }
     const updatedUsers = users.filter((u) => u.id !== id);
     setUsers(updatedUsers);
   };
 
-  const updateUserStatus = (id, status, comment) => {
-    // Reference to a specific user by userId
-    const userRef = firebase.database().ref(`users/${id}`);
+  const updateUserStatus = async (id, status, comment) => {
+    const usersRef = collection(db, "users");
+
+    // Create a query to find the document with the matching userId
+    const q = query(usersRef, where("id", "==", id));
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach(async (doc) => {
+        await updateDoc(doc.ref, {
+          status,
+          time: new Date().toLocaleTimeString(),
+          comment,
+        });
+        console.log(`Updated user with id: ${id}`);
+      });
+    } catch (e) {
+      console.error("Error updating user document: ", e);
+    }
 
     const updatedUsers = users.map((user) => {
       if (user.id === id) {
@@ -50,11 +122,6 @@ export const UserProvider = ({ children }) => {
       return user;
     });
     setUsers(updatedUsers);
-    userRef.update({
-      status,
-      time: firebase.database.ServerValue.TIMESTAMP,
-      comment,
-    });
   };
 
   return (
