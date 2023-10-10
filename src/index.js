e17dea55922603b4684f009509f21e1a2089a944
andrey94308 <android9430802@ya.ru>
2023-10-10 13:16:26 +0200

    cors v6

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import App from "./App";
 import "./index.css";
 import { createRoot } from 'react-dom/client';
 
+const functions = require('firebase-functions');
+
+exports.myFunction = functions.https.onRequest((req, res) => {
+  res.set('Content-Type', 'application/javascript');
+  res.send('console.log("Hello, world!");');
+});
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
